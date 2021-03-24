a0c67f5dd2f64310d22e8cbe5a3e588a827369be
Thiago Silva <thiago25pro@gmail.com>
2021-03-24 17:45:07 -0300

    fix: reset page to 1 on new request and prevent empty input

--- js/scripts.js
+++ js/scripts.js
@@ -19,36 +19,44 @@ inputElement.addEventListener("keyup", function (event) {
 	}
 })
 
-buttonElement.onclick = function search() {
+buttonElement.onclick = async function search() {
 	loadingElement.classList.add('reveal')
 
-	const users = getUsersFromApi(inputElement.value.toLowerCase(), page)
-	if (!users)
-		list += `Usuário não encontrado!`
-
-	if (!shouldLoadMore)
+	if (!shouldLoadMore) {
+		page = 1
 		list = ''
-	users.then(userArray => {
-		userArray.map(user => {
-			list +=
-				`<section class="user-item">
+	}
+
+	if (inputElement.value === '') {
+		loadingElement.classList.remove('reveal')
+		infoTextElement.classList.add('reveal')
+		infoTextElement.innerHTML = 'Digite um nome de usuário!'
+		return
+	}
+
+	const users = await getUsersFromApi(inputElement.value.toLowerCase(), page)
+	if (users === null) {
+		loadingElement.classList.remove('reveal')
+		infoTextElement.classList.add('reveal')
+		infoTextElement.innerHTML = 'Usuário não encontrado!'
+		return
+	}
+
+	users.map(user => {
+		list +=
+			`<section class="user-item">
         <img src="${user.avatar_url}" alt="alt">
         <p>${user.login}</p>
         <a href="https://github.com/${user.login}?tab=repositories" target="_blank" rel="noopener noreferrer">Repositórios</a>
         <a href="${user.html_url}" target="_blank" rel="noopener noreferrer">Ver perfil</a>
       </section>`
-		})
-
-		infoTextElement.innerHTML = `${inputElement.value} está seguindo os seguintes Devs`
-		infoTextElement.classList.add('reveal')
-		loadingElement.classList.remove('reveal')
-		loadMoreButton.classList.add('reveal')
-		listElement.innerHTML = list
-		shouldLoadMore = false
-
-	}).catch(error => {
-		listElement.innerHTML = `<h3>Usuário não encontrado</h3>`
 	})
+	infoTextElement.innerHTML = `${inputElement.value} está seguindo os seguintes Devs`
+	infoTextElement.classList.add('reveal')
+	loadingElement.classList.remove('reveal')
+	loadMoreButton.classList.add('reveal')
+	listElement.innerHTML = list
+	shouldLoadMore = false
 }
 
 function loadMore() {
--- js/service.js
+++ js/service.js
@@ -1,5 +1,5 @@
 async function getUsersFromApi(username, page) {
 	const response = await fetch(`https://api.github.com/users/${username}/following?per_page=4&page=${page}`)
-	if (response.status !== 200) return
+	if (response.status !== 200) return null
 	return response.json()
 }
